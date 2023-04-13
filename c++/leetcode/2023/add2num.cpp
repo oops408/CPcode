244e5687681b0ce1ca69b41e620ca9de7ef91cca
oops408 <68081409+oops408@users.noreply.github.com>
2023-04-13 00:07:28 -0700

    Create add2num.cpp

---
+++ c++/leetcode/2023/add2num.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+    ListNode* addTwoNumbers(ListNode *l1, ListNode *l2) {
+        if(l1==NULL &&l2==NULL) return l2;
+        else if(l1==NULL) return l2;
+        else if(l2==NULL) return l1;
+
+        int a =l1->val+l2->val;
+        //taking the unit digit only in consideration 
+        ListNode* p=new ListNode(a%10);
+        p->next=addTwoNumbers(l1->next,l2->next);
+        //if the sum has carry it will be not more than 1 so 
+        //we introduce a new node and add the next value of the
+        //linkedlist with the carry one 
+        if(a>=10){
+            p->next=addTwoNumbers(p->next,new ListNode(1));
+        }
+        return p;
+
+	}
+};
